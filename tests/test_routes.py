0a7110298a780a6a1d7a000be2014bddd487af95
TalhaAhmedAI <aiforeveryoneai@gmail.com>
2023-10-26 21:50:24 +0500

    Revert "Revert "Merge pull request #18 from TalhaAhmedAI/add-ci-build""
    This reverts commit d2a7867cfaa0951147ebe5e58a95934eaee1cfdc.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -174,3 +174,7 @@ class TestAccountService(TestCase):
         resp = self.client.delete(f"{BASE_URL}/{account.id}")
 
         self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+    
+    #Adding a comment to test the build
+
+    print("I'm testing the build")
